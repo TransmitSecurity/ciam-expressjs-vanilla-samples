a9aa06d4ba7b74e2443be0ff89c94c67e5d0cd03
Ron Peleg <75998795+ts-ron@users.noreply.github.com>
2023-02-19 09:11:01 +0000

    better errors on missing webauthn config

--- webauthn-cross-device/routes/desktop.js
+++ webauthn-cross-device/routes/desktop.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
   res.render("desktop", {
-    clientId: process.env.TS_CLIENT_ID
+    clientId: process.env.TS_CLIENT_ID,
+    appId: process.env.TS_APP_ID
   });
 });
 
