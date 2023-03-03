9e85618c467b340a78d7b05ed20915515628b57b
ron@transmitsecurity.com <ron@transmitsecurity.com>
2023-03-03 14:38:46 +0200

    code-ql cleanup

--- password-authentication/backend/app.js
+++ password-authentication/backend/app.js
@@ -1,6 +1,7 @@
 import express, { json, urlencoded } from 'express';
 import session from 'express-session'
 import logger from 'morgan';
+import crypto from 'crypto';
 
 import router from './routes/index';
 
@@ -12,12 +13,13 @@ app.use(urlencoded({ extended: false }));
 
 // This is a simplistic session mechanism
 // not designed to be used in production
+// This sample may be used with HTTP; in production code always use secure cooke, and add CSRF protection
 app.use(
   session({
-    secret: 'randomsecret!',
+    secret: crypto.randomUUID(),
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false },
+    cookie: { secure: false }, 
   }),
 )
 
