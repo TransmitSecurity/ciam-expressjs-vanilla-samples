9b1c79dba1e4623550a54a3ada9cb52cd52d6c58
ron@transmitsecurity.com <ron@transmitsecurity.com>
2023-03-07 16:02:06 +0200

    moved SCSS files to shared location

--- login-with-sms/pages/init.js
+++ login-with-sms/pages/init.js
@@ -1,7 +1,7 @@
 // This module places the VITE meta env on the window, as well as common functions that
 // are used in multiple samples.
 
-import "./css/style.scss";
+import "../../shared/css/style.scss";
 import { pageUtils } from "../../shared/pageUtils.js";
 
 window.env = import.meta.env;
