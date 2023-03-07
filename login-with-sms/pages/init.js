c472c649f5678870eed898fc8ee4bf0b0e31192a
ron@transmitsecurity.com <ron@transmitsecurity.com>
2023-03-07 15:49:58 +0200

    using nicer UI for login with sms example

---
+++ login-with-sms/pages/init.js
@@ -0,0 +1,8 @@
+// This module places the VITE meta env on the window, as well as common functions that
+// are used in multiple samples.
+
+import "./css/style.scss";
+import { pageUtils } from "../../shared/pageUtils.js";
+
+window.env = import.meta.env;
+window.pageUtils = pageUtils;
