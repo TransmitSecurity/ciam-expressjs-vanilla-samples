d1b011edf2880794e943608c976fe40819417175
Nadav Bueno <nadav.bueno@transmitsecurity.com>
2023-03-13 16:58:25 +0200

    Create user and login with Users API

--- password-authentication/backend/routes/index.js
+++ password-authentication/backend/routes/index.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import { loginPassword, signupPassword } from '../lib/passwords';
-import fetch from 'node-fetch';
+import { login, signUp } from '../lib/passwords';
 import { common } from '@ciam-expressjs-vanilla-samples/shared';
+import { getUser, logout } from '../lib/management.js';
 
 const router = Router();
 
 // Render home page
 router.get(['/'], async function (req, res) {
-  // TODO add error handling, omitted for sample clarity
   res.redirect('/pages/home.html');
 });
 
@@ -19,70 +18,60 @@ router.get('/complete', async function (req, res) {
 
   if (tokens.id_token) {
     const idToken = common.tokens.parseJwt(tokens.id_token);
+
     req.session.tokens = {
       accessToken: tokens.access_token,
       refreshToken: tokens.refresh_token,
       idToken,
     };
+
+    req.session.user = await getUser(idToken.sub);
     req.session.save();
   }
 
   res.redirect('/');
 });
 
-// Authenticate a user with a password
-router.post('/login', async function (req, res) {
-  // TODO add error handling, omitted for sample clarity
-  console.log(JSON.stringify(req.body));
-  const result = await loginPassword(req.body.username, req.body.password);
-
-  res.send(result);
-});
-
 // Create a user with a username and password
 router.post('/signup', async function (req, res) {
-  // TODO add error handling, omitted for sample clarity
   console.log(JSON.stringify(req.body));
   const { username, password } = req.body;
-  const result = await signupPassword(username, password);
+  const signupResponse = await signUp(username, password);
+  console.log('Signup response', signupResponse);
+
+  if (signupResponse.status >= 400) {
+    res.send(signupResponse);
+  } else {
+    const result = await login(username, password);
+    res.send(result);
+  }
+});
+
+// Authenticate a user with a password
+router.post('/login', async function (req, res) {
+  console.log(JSON.stringify(req.body));
+  const { username, password } = req.body;
+
+  const result = await login(username, password);
   res.send(result);
 });
 
-// Get an authenticated user's saved ID Token or return a not found error
-// SECURITY NOTES: Normally the ID token SHOULD NOT reach the UI, we send and display it here for clarity.
+// Get an authenticated user's data
 router.get('/user', async function (req, res) {
-  // TODO add error handling, omitted for sample clarity
-  console.log('/user', req.session.tokens);
-  if (req.session.tokens) {
-    res.status(200).send({
-      idToken: req.session.tokens.idToken,
-    });
+  console.log('/user', req.session.user);
+  if (req.session.user) {
+    res.status(200).send(req.session.user);
   } else {
-    res.status(404).send({
-      idToken: null,
-    });
+    res.status(404).send(null);
   }
 });
 
 // Logout user
 router.post('/logout', async function (req, res) {
   const accessToken = req.session.tokens.accessToken;
-  const url = common.config.apis.logout;
-
-  req.session.tokens = undefined;
-  req.session.save();
-
-  const data = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-
-  const json = await data.json();
-  console.log('Logout', json);
-
-  res.send(json);
+  req.session.destroy(err => console.log(err));
+  const result = await logout(accessToken);
+  res.send(result);
 });
 
 export default router;
