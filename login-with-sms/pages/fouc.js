3d92a78cb7929c7b5f57eafab2ff4427a8a87f92
Ron Peleg <75998795+ts-ron@users.noreply.github.com>
2023-03-20 15:48:31 +0000

    removed fouc.js, compacted all into init.js

--- login-with-sms/pages/fouc.js
+++
@@ -1,5 +0,0 @@
-const elm = document.getElementsByTagName('html')[0];
-elm.style.display = 'none';
-document.addEventListener('DOMContentLoaded', function (event) {
-  elm.style.display = 'block';
-});
--- webauthn-cross-device/pages/init.js
+++ webauthn-cross-device/pages/init.js
@@ -1,8 +1,12 @@
 // This module places the VITE meta env on the window, as well as common functions that
 // are used in multiple samples.
-
 import { pageUtils } from '../../shared/pageUtils.js';
 import '../../shared/css/style.scss';
+
+// this together with <html style="display: none"> avoids flash of unstyled content (FOUC)
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementsByTagName('html')[0].style.display = 'block';
+});
 
 window.env = import.meta.env;
 window.pageUtils = pageUtils;
