d5d59fabecb0367a1711583d2ffba1fedeb85413
ron@transmitsecurity.com <ron@transmitsecurity.com>
2023-04-27 12:23:30 +0300

    temporary revert to userid domain until we have the gateway fix

--- authentication-hub/backend/lib/passwords.js
+++ authentication-hub/backend/lib/passwords.js
@@ -39,19 +39,22 @@ export async function login(username, password) {
     process.env.TS_CLIENT_SECRET_SSOHUB,
   );
 
-  const response = await fetch(common.config.apis.passwordLogin, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${clientToken}`,
+  const response = await fetch(
+    `https://api.userid.security/v1/auth/password/login` /*common.config.apis.passwordLogin*/,
+    {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${clientToken}`,
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        client_id: process.env.VITE_TS_CLIENT_ID_SSOHUB,
+        redirect_uri: process.env.TS_REDIRECT_URI_SSOHUB,
+      }),
     },
-    body: JSON.stringify({
-      username,
-      password,
-      client_id: process.env.VITE_TS_CLIENT_ID_SSOHUB,
-      redirect_uri: process.env.TS_REDIRECT_URI_SSOHUB,
-    }),
-  });
+  );
 
   const jsonResponse = await response.json();
 
