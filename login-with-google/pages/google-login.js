1d80d0a057d898e0c241c2c50b0c117d10773c08
Hila Partuk <hila.partuk@transmitsecurity.com>
2023-06-20 12:32:30 +0300

    MND-7680 # init demo login with google

---
+++ login-with-google/pages/google-login.js
@@ -0,0 +1,8 @@
+async function startGoogleLogin(handleResponse) {
+    const response = await fetch('/google-login', {method: 'GET'});
+
+    const status = response.status;
+    const data = await response.json();
+    console.log('Response from /google-login', { status, data });
+    handleResponse({ status, data });
+}
