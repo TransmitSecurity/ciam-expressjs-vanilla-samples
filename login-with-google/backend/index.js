6f02056728a46589e8276e525fe8847a05b2fab6
Hila Partuk <hila.partuk@transmitsecurity.com>
2023-06-20 16:43:03 +0300

    MND-7680 # adding index-router

--- login-with-google/backend/index.js
+++ login-with-google/backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {common} from "@ciam-expressjs-vanilla-samples/shared";
+import { common } from '@ciam-expressjs-vanilla-samples/shared';
 
 const router = express.Router();
 
@@ -7,55 +7,50 @@ const router = express.Router();
  * For more information see https://developer.transmitsecurity.com/guides/user/auth_google/
  * **/
 
-
 // GET login page
 router.get('/', function (req, res) {
-    res.redirect('/pages/google-login.html');
+  res.redirect('/pages/google-login.html');
 });
 
-router.get('/google-login',
-    common.utils.rateLimiter(),
-    async function (req, res) {
-        try {
-            const googleLoginResponse = await googleLogin();
+router.get('/google-login', common.utils.rateLimiter(), async function (req, res) {
+  try {
+    const googleLoginResponse = await googleLogin();
 
-            res.status(googleLoginResponse.status).send({
-                ...googleLoginResponse,
-            });
-
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({
-                message: 'Error in the google-login flow',
-                error,
-            });
-        }
+    res.status(googleLoginResponse.status).send({
+      ...googleLoginResponse,
     });
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: 'Error in the google-login flow',
+      error,
+    });
+  }
+});
 
 async function googleLogin() {
-    const googleLoginUrl = common.config.apis.googleLogin;
+  const googleLoginUrl = common.config.apis.googleLogin;
 
-    const queryParams = new URLSearchParams({
-        client_id: process.env.VITE_TS_CLIENT_ID,
-        redirect_uri: process.env.TS_REDIRECT_URI,
-        create_new_user: true,
-    });
+  const queryParams = new URLSearchParams({
+    client_id: process.env.VITE_TS_CLIENT_ID,
+    redirect_uri: process.env.TS_REDIRECT_URI,
+    create_new_user: true,
+  });
 
-    const url = `${googleLoginUrl}?${queryParams.toString()}`;
-    console.log('about to call ' + url);
+  const url = `${googleLoginUrl}?${queryParams.toString()}`;
+  console.log('about to call ' + url);
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-        },
-    });
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
 
-    const status = resp.status;
-    const data = await response.json();
-    
-    console.log('response is ', {status, data});
-    return {status, ...data};
+  const status = response.status;
+  const data = await response.json();
+
+  console.log('response is ', { status, data });
+  return { status, ...data };
 }
-
+export const indexRouter = router;
--- login-with-google/pages/google-login.js
+++ login-with-google/pages/google-login.js
@@ -1,8 +1,9 @@
+/*
 async function startGoogleLogin(handleResponse) {
-    const response = await fetch('/google-login', {method: 'GET'});
+  const response = await fetch('/google-login', {method: 'GET'});
 
-    const status = response.status;
-    const data = await response.json();
-    console.log('Response from /google-login', { status, data });
-    handleResponse({ status, data });
-}
+  const status = response.status;
+  const data = await response.json();
+  console.log('Response from /google-login', { status, data });
+  handleResponse({ status, data });
+}*/
