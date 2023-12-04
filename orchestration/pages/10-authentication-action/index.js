04ef4cef6cde02c3395969953e79ca46eaef44fc
Shay Kligerman <shay.kligerman@transmitsecurity.com>
2023-12-04 13:18:07 +0200

    FLOP-87: Authenticate action

--- orchestration/pages/10-authentication-action/index.js
+++ orchestration/pages/10-authentication-action/index.js
@@ -1,3 +1,4 @@
+import { pageUtils } from '../../../shared/pageUtils.js';
 import { executeJourney, flowId, showInformation } from '../commonUtils.js';
 import { ClientResponseOptionType, IdoJourneyActionType } from '../sdk_interface.js';
 
@@ -27,7 +28,14 @@ if (parsedState && parsedState.expires > new Date().getTime()) {
 }
 
 function onClick() {
-  executeJourney(JOURNEY_NAME, handleJourneyActionUI, JOURNEY_ADDITIONAL_PARAMS);
+  executeJourney(
+    JOURNEY_NAME,
+    handleJourneyActionUI,
+    JOURNEY_ADDITIONAL_PARAMS,
+    undefined,
+    undefined,
+    { webauthn: { serverPath: 'https://api.idsec-stg.com' } },
+  );
 }
 
 async function handleJourneyActionUI(idoResponse) {
@@ -50,13 +58,10 @@ async function handleJourneyActionUI(idoResponse) {
       clientResponse = await handleCollectUsernameForm();
       break;
     case IdoJourneyActionType.Authentication:
-      clientResponse = await showInformation({
+      clientResponse = await showAuthentication({
         title: 'Authenticate with Webauthn action',
         text: 'About to authenticate using a webauthn key',
       });
-      clientResponse.data.webauthn_encoded_result =
-        await window.tsPlatform.webauthn.authenticate.modal(actionData.username);
-      clientResponse.data.type = 'webauthn';
       break;
     default:
       throw `Unexpected step id: ${stepId}`;
@@ -88,5 +93,63 @@ function handleCollectUsernameForm() {
     );
 
     document.getElementById('collect_username').style.display = 'block';
+  });
+}
+
+async function showAuthentication(actionData) {
+  return new Promise((resolve /*reject*/) => {
+    async function submit() {
+      const webauthn_encoded_result = await window.tsPlatform.webauthn.authenticate.modal(
+        actionData.username,
+      );
+      pageUtils.hide('authentication_form');
+      pageUtils.hide('action_response_error');
+      resolve({
+        option: ClientResponseOptionType.ClientInput,
+        data: {
+          webauthn_encoded_result,
+          type: 'webauthn',
+        },
+      });
+    }
+
+    function escape() {
+      pageUtils.hide('authentication_form');
+      pageUtils.hide('action_response_error');
+      resolve({
+        option: 'escape_1',
+        data: {},
+      });
+    }
+    function cancel() {
+      pageUtils.hide('authentication_form');
+      pageUtils.hide('action_response_error');
+      resolve({
+        option: ClientResponseOptionType.Cancel,
+        data: {},
+      });
+    }
+
+    pageUtils.updateElementText(
+      'authentication_form_title',
+      actionData?.title || 'Empty title from server',
+    );
+    pageUtils.updateElementText(
+      'authentication_form_text',
+      actionData?.text || 'Empty text from server',
+    );
+    pageUtils.updateElementText('authenticate_button', actionData?.button_text || 'OK');
+    pageUtils.show('authentication_form');
+
+    // clear all handlers, this handles multiple runs of the same action
+    document.querySelector('#authenticate_button').removeEventListener('click', submit);
+    document.querySelector('#escape_button').removeEventListener('click', escape);
+    document.querySelector('#cancel_button').removeEventListener('click', cancel);
+
+    // Handle input field and main submit
+    // eslint-disable-next-line no-unused-vars
+    document.querySelector('#authenticate_button').addEventListener('click', submit);
+    document.querySelector('#escape_button').addEventListener('click', escape);
+    document.querySelector('#cancel_button').addEventListener('click', cancel);
   });
 }
