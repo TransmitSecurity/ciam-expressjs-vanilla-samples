f37f94db213df76fb76c762d930322d5449f9945
Idan Berman <idan.berman@transmitsecurity.com>
2023-12-17 11:52:05 +0200

    clean

--- hosted-webauthn-for-logged-out-users/vite.config.js
+++ hosted-webauthn-for-logged-out-users/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import mix from 'vite-plugin-mix';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '../.env.stage' }); // load env vars from .env
+dotenv.config({ path: '../.env' }); // load env vars from .env
 
 export default defineConfig({
   plugins: [
